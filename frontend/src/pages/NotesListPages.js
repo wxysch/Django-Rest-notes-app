37386b42aee241c8998e9cfc652a2ef33ad8ec1b
alinu1 <uwu1343@gmail.com>
2023-03-10 15:29:35 +0600

    making api calls

--- frontend/src/pages/NotesListPages.js
+++ frontend/src/pages/NotesListPages.js
@@ -4,6 +4,18 @@ const NotesListPages = () => {
 
     let [notes,setNotes] = useState([])
 
+    useEffect(()=> {
+
+    }, [])
+
+
+    let getNotes = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let data = await response.json()
+        console.log('DATA',data)
+        setNotes(data)
+    }
+
   return (
     <div>
         notes
