d89e4fc13303ee9a2a6856e9bf723408c8d9df31
alinu1 <uwu1343@gmail.com>
2023-03-10 14:24:20 +0600

    set up react project

---
+++ frontend/src/pages/NotesListPages.js
@@ -0,0 +1,14 @@
+import React, {useState,useEffect} from 'react'
+
+const NotesListPages = () => {
+
+    let [notes,setNotes] = useState([])
+
+  return (
+    <div>
+        notes
+    </div>
+  )
+}
+
+export default NotesListPages
