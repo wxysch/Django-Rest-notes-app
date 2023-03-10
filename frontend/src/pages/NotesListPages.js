4f6a467a295f7a120c3eddea955fb054d25d29c1
alinu1 <uwu1343@gmail.com>
2023-03-11 00:51:36 +0600

    CORS fix

--- frontend/src/pages/NotesListPages.js
+++ frontend/src/pages/NotesListPages.js
@@ -1,24 +1,28 @@
 import React, {useState,useEffect} from 'react'
+import ListItem from '../components/ListItem'
 
 const NotesListPages = () => {
 
     let [notes,setNotes] = useState([])
 
     useEffect(()=> {
-
+      getNotes()
     }, [])
 
 
-    let getNotes = async () => {
-        let response = await fetch('http://127.0.0.1:8000/api/notes/')
-        let data = await response.json()
-        console.log('DATA',data)
-        setNotes(data)
-    }
+  let getNotes = async () => {
+      let response = await fetch('http://127.0.0.1:8000/api/notes/')
+      let data = await response.json()
+      setNotes(data)
+      }
 
   return (
     <div>
-        notes
+        <div className='notes-list'>
+            {notes.map((note,index) => (
+              <ListItem key={index} note={note}/>
+            ))}
+        </div>
     </div>
   )
 }
--- mynotes/settings.py
+++ mynotes/settings.py
@@ -25,12 +25,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #libs
     'rest_framework',
+    'corsheaders',
+
+    #apps
     'api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,3 +116,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
